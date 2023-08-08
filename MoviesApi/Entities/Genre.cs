b4a402eebbad28ceae0c4363f64786ed22621382
Niaz Murshed Siam <niazmurshed16@gmail.com>
2023-08-08 13:40:46 +0600

    Custom Validation

--- MoviesApi/Entities/Genre.cs
+++ MoviesApi/Entities/Genre.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoviesApi.Validations;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,12 +7,25 @@ using System.Threading.Tasks;
 
 namespace MoviesApi.Entities
 {
-    public class Genre
+    public class Genre: IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage="The Feild Name Required")]
         [StringLength(20)]
+       // [FirstLetterUpperCaseAttribute]
         public string Name { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                var firstLetter = Name[0].ToString();
+                if (firstLetter != firstLetter.ToUpper())
+                {
+                    yield return new ValidationResult("First Letter Should be UpperCase", new string[] { nameof(Name) });
+                }
+            }
+        }
         //[Range(10,100)]
         //public int Age { get; set; }
         //[CreditCard]
---
+++ MoviesApi/Validations/FirstLetterUpperCaseAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApi.Validations
+{
+    public class FirstLetterUpperCaseAttribute: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if(value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+            var firstName = value.ToString()[0].ToString();
+            if(firstName != firstName.ToString())
+            {
+                return new ValidationResult("First Letter Should be UpperCase");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
